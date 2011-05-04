32cdb7d70c9cddd8e5a6f4ca40c22d8946faa18e
PaulWagener <mail@paulwagener.nl>
2011-05-04 16:13:59 +0000

    Nice NetBeans package
    git-svn-id: http://streetview-explorer.googlecode.com/svn/trunk@19 a12dd914-4bc0-b5ed-ed5a-5c41bd50ae82

---
+++ mac/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=mac
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/mac
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=mac.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/mac.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=mac
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/mac
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=mac.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/mac.tar
